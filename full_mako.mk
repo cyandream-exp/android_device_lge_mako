061cdb47a8651f27a1159d12f0b02353f5005a25
yanniks <kontakt@yanniks.de>
2013-11-04 07:27:35 +0100

    use our vendor

--- full_mako.mk
+++ full_mako.mk
@@ -25,7 +25,9 @@ PRODUCT_PACKAGES += \
 
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+#PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+$(call inherit-product, vendor/kidream/configs/common.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
